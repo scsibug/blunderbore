aa2b30c6ec73d7092c737d49141d9560c4f7d64a
Greg Heartsfield <scsibug@imap.cc>
2010-06-02 20:56:39 -0500

    Tube stats work.

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.List(intersperse)
+import qualified Data.Map as M
 import Control.Monad
 import Control.Monad.Trans(liftIO)
 import Happstack.Server (nullConf, simpleHTTP, ok, dir, path)
@@ -8,7 +9,7 @@ import Network.Beanstalk
 
 main :: IO ()
 main = do bs <- connectBeanstalk "localhost" "11300"
-          simpleHTTP nullConf $ msum [dir "tube" $ path $ \tubename -> ok $ "Tube "++tubename
+          simpleHTTP nullConf $ msum [dir "tube" $ path $ \tubename -> (tubeInfoStr bs tubename)
                                      ,dir "tube" (tubeListStr bs)
                                      ,ok "Use /tube/tubename"
                                      ]
@@ -16,3 +17,8 @@ main = do bs <- connectBeanstalk "localhost" "11300"
 
 tubeListStr bs = do lt <- liftIO (listTubes bs)
                     ok (foldr (++) "" (intersperse "\n" lt))
+
+tubeInfoStr bs tubename = do stats <- liftIO (statsTube bs tubename)
+                             let kv = M.assocs stats
+                             let statslist = map (\(k,v) -> (k ++ " => " ++ v)) kv
+                             ok (foldr (++) "" (intersperse "\n" statslist))
