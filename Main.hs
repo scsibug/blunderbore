978547f64b6af3b2dfa94c2ababa34bf8465aa70
Greg Heartsfield <scsibug@imap.cc>
2010-06-02 19:59:02 -0500

    routing

--- Main.hs
+++ Main.hs
@@ -5,6 +5,6 @@ import Happstack.Server (nullConf, simpleHTTP, ok, dir, path)
 
 main :: IO ()
 main = simpleHTTP nullConf $ msum [dir "tube" $ path $ \tubename -> ok $ "Tube "++tubename
-                                  ,dir "tube" $ ok "Hello World!"
-                                  ,ok "Root or error"
+                                  ,dir "tube" $ ok "Main tube listing"
+                                  ,ok "Use /tube/tubename"
                                   ]
