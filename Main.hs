6827974e6b35ba969699d874a55f514e383cd3dc
Greg Heartsfield <scsibug@imap.cc>
2010-06-02 20:37:37 -0500

    Listing tubes works.

--- Main.hs
+++ Main.hs
@@ -1,10 +1,18 @@
 module Main where
 
+import Data.List(intersperse)
 import Control.Monad
+import Control.Monad.Trans(liftIO)
 import Happstack.Server (nullConf, simpleHTTP, ok, dir, path)
+import Network.Beanstalk
 
 main :: IO ()
-main = simpleHTTP nullConf $ msum [dir "tube" $ path $ \tubename -> ok $ "Tube "++tubename
-                                  ,dir "tube" $ ok "Main tube listing"
-                                  ,ok "Use /tube/tubename"
-                                  ]
+main = do bs <- connectBeanstalk "localhost" "11300"
+          simpleHTTP nullConf $ msum [dir "tube" $ path $ \tubename -> ok $ "Tube "++tubename
+                                     ,dir "tube" (tubeListStr bs)
+                                     ,ok "Use /tube/tubename"
+                                     ]
+
+
+tubeListStr bs = do lt <- liftIO (listTubes bs)
+                    ok (foldr (++) "" (intersperse "\n" lt))
