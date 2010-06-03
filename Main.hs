7cd4de07b84bcdaa45524aad7c90b02b700fdbb1
Greg Heartsfield <scsibug@imap.cc>
2010-06-02 21:02:12 -0500

    Server stats work.

--- Main.hs
+++ Main.hs
@@ -12,9 +12,14 @@ main = do bs <- connectBeanstalk "localhost" "11300"
           simpleHTTP nullConf $ msum [dir "tube" $ path $ \tubename
                                           -> (tubeInfoStr bs tubename)
                                      ,dir "tube" (tubeListStr bs)
-                                     ,ok "Use /tube/tubename"
+                                     ,serverStatStr bs
                                      ]
 
+
+serverStatStr bs = do stats <- liftIO (statsServer bs)
+                      let kv = M.assocs stats
+                      let statslist = map (\(k,v) -> (k ++ " => " ++ v)) kv
+                      ok (foldr (++) "" (intersperse "\n" statslist))
 
 tubeListStr bs = do lt <- liftIO (listTubes bs)
                     ok (foldr (++) "" (intersperse "\n" lt))
