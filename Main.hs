7a6fd191852ce6a6964971ad09c8ed117bcd50ad
Greg Heartsfield <scsibug@imap.cc>
2010-06-02 22:29:22 -0500

    Using xhtml to display server stats.

--- Main.hs
+++ Main.hs
@@ -4,17 +4,30 @@ import Data.List(intersperse)
 import qualified Data.Map as M
 import Control.Monad
 import Control.Monad.Trans(liftIO)
-import Happstack.Server (nullConf, simpleHTTP, ok, dir, path)
+import Happstack.Server (nullConf, simpleHTTP, ok, dir, path, ServerPart, toResponse, Response)
+import Text.XHtml.Transitional hiding (dir)
 import Network.Beanstalk
 
 main :: IO ()
 main = do bs <- connectBeanstalk "localhost" "11300"
-          simpleHTTP nullConf $ msum [dir "tube" $ path $ \tubename
-                                          -> (tubeInfoStr bs tubename)
-                                     ,dir "tube" (tubeListStr bs)
-                                     ,serverStatStr bs
+          simpleHTTP nullConf $ msum [--dir "tube" $ path $ \tubename
+--                                          -> (tubeInfoStr bs tubename)
+--                                     ,dir "tube" (tubeListStr bs)
+                                     showServerStats bs --serverStatStr bs
                                      ]
 
+showServerStats :: BeanstalkServer -> ServerPart Response
+showServerStats bs = do stats <- liftIO (statsServer bs)
+                        ok $ toResponse $ statsHtml stats
+
+statsHtml :: M.Map String String -> Html
+statsHtml stats = body (concatHtml [h3 (stringToHtml "Server Stats"),statTable])
+    where
+      statTable = table (concatHtml rows)
+      rows = map (\(k,v) -> tr (concatHtml [(td (stringToHtml k)), (td (stringToHtml v))])) kv
+--      statStr = (foldr (++) "" (intersperse "\n" statslist))
+--      statslist = map (\(k,v) -> (k ++ " => " ++ v)) kv
+      kv = M.assocs stats
 
 serverStatStr bs = do stats <- liftIO (statsServer bs)
                       let kv = M.assocs stats
@@ -28,3 +41,4 @@ tubeInfoStr bs tubename = do stats <- liftIO (statsTube bs tubename)
                              let kv = M.assocs stats
                              let statslist = map (\(k,v) -> (k ++ " => " ++ v)) kv
                              ok (foldr (++) "" (intersperse "\n" statslist))
+
