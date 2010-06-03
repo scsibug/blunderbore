abaa82472818b3f3f1016b7727c102061baf8215
Greg Heartsfield <scsibug@imap.cc>
2010-06-02 19:57:29 -0500

    Path-based routing.

--- Main.hs
+++ Main.hs
@@ -1,10 +1,10 @@
 module Main where
 
 import Control.Monad
-import Happstack.Server (nullConf, simpleHTTP, ok, dir)
+import Happstack.Server (nullConf, simpleHTTP, ok, dir, path)
 
 main :: IO ()
-main = simpleHTTP nullConf $ msum [mzero
-                                  , ok "Hello World!"
-                                  , ok "Unreachable ServerPartT"
+main = simpleHTTP nullConf $ msum [dir "tube" $ path $ \tubename -> ok $ "Tube "++tubename
+                                  ,dir "tube" $ ok "Hello World!"
+                                  ,ok "Root or error"
                                   ]
