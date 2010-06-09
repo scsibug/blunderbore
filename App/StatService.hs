6a2e6e417f1c9298fe064f22a50715a330492b07
Greg Heartsfield <scsibug@imap.cc>
2010-06-08 23:47:47 -0500

    Type signature and header comment block for App.StatService.

--- App/StatService.hs
+++ App/StatService.hs
@@ -1,13 +1,25 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Blunderbore App StatService
+-- Copyright   :  (c) Greg Heartsfield 2010
+-- License     :  BSD3
+--
+-- Scheduled service for updating Beanstalk stats.
+--
+-----------------------------------------------------------------------------
+
 module App.StatService (updateStatsService) where
 
 import Happstack.Data
 import Happstack.State
 import Data.Data (Data)
+import Control.Concurrent (MVar)
 import System.Log.Logger (Priority(..), logM)
 import Network.Beanstalk
 import App.State
 
---updateStatsService :: MVar TxControl -> BeanstalkServer -> IO ()
+
+updateStatsService :: MVar TxControl -> BeanstalkServer -> IO ()
 updateStatsService state bs =
     do st <- statsServer bs
        update $ AddStat 4
